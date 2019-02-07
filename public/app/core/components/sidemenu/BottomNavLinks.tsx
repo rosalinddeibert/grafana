b8ec9fffd6e29ae1129b88de3cf694b51a0b6495
Dave Osborne <osborne.dave@gmail.com>
2019-02-07 12:50:27 -0500

    update look and feel of dark (default) theme

--- public/app/core/components/sidemenu/BottomNavLinks.tsx
+++ public/app/core/components/sidemenu/BottomNavLinks.tsx
@@ -7,6 +7,11 @@ export interface Props {
   user: User;
 }
 
+const rightMenuStyle = {
+  right: 0,
+  left: 'auto',
+};
+
 class BottomNavLinks extends PureComponent<Props> {
   itemClicked = (event, child) => {
     if (child.url === '/shortcuts') {
@@ -26,14 +31,14 @@ class BottomNavLinks extends PureComponent<Props> {
   render() {
     const { link, user } = this.props;
     return (
-      <div className="sidemenu-item dropdown dropup">
+      <div className="sidemenu-item dropdown">
         <a href={link.url} className="sidemenu-link" target={link.target}>
           <span className="icon-circle sidemenu-icon">
             {link.icon && <i className={link.icon} />}
             {link.img && <img src={link.img} />}
           </span>
         </a>
-        <ul className="dropdown-menu dropdown-menu--sidemenu" role="menu">
+        <ul className="dropdown-menu dropdown-menu--sidemenu" style={rightMenuStyle} role="menu">
           {link.subTitle && (
             <li className="sidemenu-subtitle">
               <span className="sidemenu-item-text">{link.subTitle}</span>
