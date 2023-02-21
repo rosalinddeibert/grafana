569518a1c71b4e9922629c0e67bd6d194c288394
rosalinddeibert <rosalind.deibert@mail.utoronto.ca>
2023-02-21 17:21:37 -0500

    remove unused items from footer causing problems

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { LinkTarget } from '@grafana/data';
 import { config } from '@grafana/runtime';
-import { Icon, IconName } from '@grafana/ui';
+import { IconName } from '@grafana/ui'; // Psiphon change remove unused import
 import { t } from 'app/core/internationalization';
 
 export interface FooterLink {
@@ -107,18 +107,4 @@ export const Footer = React.memo(({ customLinks }: Props) => {
 
 Footer.displayName = 'Footer';
 
-function FooterItem({ item }: { item: FooterLink }) {
-  const content = item.url ? (
-    <a href={item.url} target={item.target} rel="noopener noreferrer" id={item.id}>
-      {item.text}
-    </a>
-  ) : (
-    item.text
-  );
-
-  return (
-    <>
-      {item.icon && <Icon name={item.icon} />} {content}
-    </>
-  );
-}
+// Psiphon change remove footer function
