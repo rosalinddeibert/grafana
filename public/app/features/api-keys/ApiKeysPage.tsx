9555e7bd0e4a62bc143e6d0b35dd99fe8fe21f86
rosalinddeibert <rosalind.deibert@mail.utoronto.ca>
2023-02-21 16:54:57 -0500

    reverting icon change

--- public/app/features/api-keys/ApiKeysPage.tsx
+++ public/app/features/api-keys/ApiKeysPage.tsx
@@ -190,7 +190,7 @@ export class ApiKeysPageUnconnected extends PureComponent<Props, State> {
                   {showCTA ? (
                     <EmptyListCTA
                       title="You haven't added any API keys yet."
-                      buttonIcon="gicon gicon-apikeys gicon--call-to-action" //Psiphon change
+                      buttonIcon="key-skeleton-alt"
                       onClick={toggleIsAdding}
                       buttonTitle="New API key"
                       proTip="Remember, you can provide view-only API access to other applications."
