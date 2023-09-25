cd195eb4a638227993d9c4e5867cb14f1f458023
rosalinddeibert <rosalind.deibert@mail.utoronto.ca>
2023-09-25 15:03:53 -0400

    merge Psiphon changes onto Grafana 10.0.6

--- public/app/core/components/Login/LoginLayout.tsx
+++ public/app/core/components/Login/LoginLayout.tsx
@@ -6,7 +6,7 @@ import { useStyles2, styleMixins } from '@grafana/ui';
 
 import { Branding } from '../Branding/Branding';
 import { BrandingSettings } from '../Branding/types';
-import { Footer } from '../Footer/Footer';
+// Psiphon change - Remove unused import after Psiphon changes
 
 interface InnerBoxProps {
   enterAnimation?: boolean;
@@ -21,33 +21,32 @@ export interface LoginLayoutProps {
   branding?: BrandingSettings;
 }
 
+// Psiphon change - remove unused/removed items from LoginLayout
 export const LoginLayout = ({ children, branding }: React.PropsWithChildren<LoginLayoutProps>) => {
   const loginStyles = useStyles2(getLoginStyles);
   const [startAnim, setStartAnim] = useState(false);
   const subTitle = branding?.loginSubtitle ?? Branding.GetLoginSubTitle();
-  const loginTitle = branding?.loginTitle ?? Branding.LoginTitle;
-  const loginBoxBackground = branding?.loginBoxBackground || Branding.LoginBoxBackground();
   const loginLogo = branding?.loginLogo;
 
   useEffect(() => setStartAnim(true), []);
 
+  // Psiphon changes - remove footer links from login page, remove loginboxbackground
+  // styling because it looks bad with custom image, remove 'Welcome to Grafana' message
   return (
     <Branding.LoginBackground
       className={cx(loginStyles.container, startAnim && loginStyles.loginAnim, branding?.loginBackground)}
     >
       <div className={loginStyles.loginMain}>
-        <div className={cx(loginStyles.loginContent, loginBoxBackground, 'login-content-box')}>
+        <div className={cx(loginStyles.loginContent, 'login-content-box')}>
           <div className={loginStyles.loginLogoWrapper}>
             <Branding.LoginLogo className={loginStyles.loginLogo} logo={loginLogo} />
             <div className={loginStyles.titleWrapper}>
-              <h1 className={loginStyles.mainTitle}>{loginTitle}</h1>
               {subTitle && <h3 className={loginStyles.subTitle}>{subTitle}</h3>}
             </div>
           </div>
           <div className={loginStyles.loginOuterBox}>{children}</div>
         </div>
       </div>
-      {branding?.hideFooter ? <></> : <Footer customLinks={branding?.footerLinks} />}
     </Branding.LoginBackground>
   );
 };
@@ -84,6 +83,7 @@ export const getLoginStyles = (theme: GrafanaTheme2) => {
       alignItems: 'center',
       justifyContent: 'center',
     }),
+    // Psiphon change - shift login box up a bit to look better with custom background image
     loginAnim: css`
       &:before {
         opacity: 1;
@@ -91,6 +91,7 @@ export const getLoginStyles = (theme: GrafanaTheme2) => {
 
       .login-content-box {
         opacity: 1;
+        margin-top: -10em;
       }
     `,
     submitButton: css`
